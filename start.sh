17fad423e54aa33b7d359c57ba8cb230ffbb2a1a
Linus187erz <linus@schoeler.de>
2024-05-16 17:39:16 +0200

    made start usable on mac, i use arch btw

--- start.sh
+++ start.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
-python -m venv venv
+
+pytn=$(which python)
+pytn3=$(which python3)
+
+if [ -z $pytn ]; then
+	if [ -z $pytn3 ]; then
+		echo "Python is not installed"
+		exit 1
+	else
+		pytn=$pytn3
+	fi
+fi
+
+$pytn -m venv venv
 source venv/bin/activate
 pip install torch torchvision
 python benchy.py
